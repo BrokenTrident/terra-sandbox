f695c1e846d0f97d63160ea4f3b8a43598876050
Christopher Gill <Chrisgill313@gmail.com>
2023-09-02 13:00:49 -0400

    updates to code

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  shared_config_files      = ["/Users/tf_user/.aws/conf"]
-  shared_credentials_files = ["~/.aws/creds"]
+  shared_config_files      = ["~/.aws/conf"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "vscode"
 }
