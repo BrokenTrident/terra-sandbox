013836d2fda85686d1d2e75eb034cceeba3f0d20
Christopher Gill <Chrisgill313@gmail.com>
2023-09-02 12:28:19 -0400

    terra 1

--- providers.tf
+++ providers.tf
@@ -6,3 +6,10 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["~/.aws/creds"]
+  profile                  = "vscode"
+}
